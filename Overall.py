21b9e10e0c84a7a9f57e8fada9b801a798c2241b
Matzin <142700606+matbatata@users.noreply.github.com>
2025-02-16 11:44:48 -0300

    Update Overall.py

--- Overall.py
+++ Overall.py
@@ -2,20 +2,26 @@ print("1.Ataque")
 print("2.Meio-campo")
 print("3.Defensor")
 print("4.Goleiro")
-p = int(input("Escolha uma posicao: "))
-fis = int(input("Digite seu fisico: "))
-pas = int(input("Digite seu passe: "))
-chu = int(input("Digite seu chute: "))
-con = int(input("Digite sua conducao: "))
-vel = int(input("Digite sua velocidade: "))
-over = (fis + pas + chu + con + vel / 5)  
-if p == 1:
-    print("Você é um atacante com",over,"de pontuacao geral")
-elif p == 2:
-    print("Você é um meio-campista com",over,"de pontuacao geral")
-elif p == 3:
-    print("Você é um defensor com",over,"de pontuacao geral")
-elif p == 4:
-    print("Você é um goleiro com",over,"de pontuacao geral")
+print("5.Sair")
+p = int(input("Escolha uma opção: "))
+if p == 5:
+    print("Saindo... até logo!")
 else:
-    print("Pontuacao geral nao identifacada")  
+    fis = int(input("Digite seu fisico: "))
+    pas = int(input("Digite seu passe: "))
+    chu = int(input("Digite seu chute: "))
+    con = int(input("Digite sua condução: "))
+    vel = int(input("Digite sua velocidade: "))
+    over = (fis + pas + chu + con + vel) / 5  
+    if p == 1:
+        print("Você é um atacante com", over, "de pontuação geral")
+    elif p == 2:
+        print("Você é um meio-campista com", over, "de pontuação geral")
+    elif p == 3:
+        print("Você é um defensor com", over, "de pontuação geral")
+    elif p == 4:
+        print("Você é um goleiro com", over, "de pontuação geral")
+    else:
+        print("Posição inválida")
+# Adicionei a saida no codigo ( pedi uma sugestão pro chat e falou para colocar um else
+# nas variaveis) 
